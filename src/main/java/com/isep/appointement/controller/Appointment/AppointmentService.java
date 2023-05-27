7db03d2bed829ef33032c01be655ee272a940f63
zhangkexindexiaobaobei <zhangkex1218@gmail.com>
2023-05-27 15:38:30 +0200

    可欣。appointement 和 apartment

---
+++ src/main/java/com/isep/appointement/controller/Appointment/AppointmentService.java
@@ -0,0 +1,23 @@
+package com.isep.appointement.controller.Appointment;
+
+
+import com.isep.appointment.model.Appointment;
+import com.isep.appointment.repository.AppointmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AppointmentService {
+    private final AppointmentRepository appointmentRepository;
+
+    @Autowired
+    public AppointmentService(AppointmentRepository appointmentRepository) {
+        this.appointmentRepository = appointmentRepository;
+    }
+
+    public Appointment createAppointment(Appointment appointment) {
+        return appointmentRepository.save(appointment);
+    }
+
+    // other methods ...
+}
---
+++ src/main/java/com/isep/appointement/model/Department.java
@@ -0,0 +1,52 @@
+package com.isep.appointement.model;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "departments")
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private int id;
+
+    @Column(name = "name", nullable = false, length = 50)
+    private String name;
+
+    @OneToMany(mappedBy = "department", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Set<Doctor> doctors;
+
+    public Department() {
+    }
+
+    public Department(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Doctor> getDoctors() {
+        return doctors;
+    }
+
+    public void setDoctors(Set<Doctor> doctors) {
+        this.doctors = doctors;
+    }
+}
